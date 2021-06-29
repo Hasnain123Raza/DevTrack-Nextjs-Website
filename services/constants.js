ae3de0d10a35a7e2f4f6447bc690992c9a930948
Hasnain Raza <hasnain.mohsan@gmail.com>
2021-06-29 13:25:16 -0400

    Changed HOST for testing

--- services/constants.js
+++ services/constants.js
@@ -1,2 +1,2 @@
-export const HOST = "https://devtrack-roblox.herokuapp.com/";
+export const HOST = "https://devtrack-roblox.herokuapp.com";
 export const SITE_KEY = "6LeGglAbAAAAACPp0i_M0GfMlSWZfS4AeQzYjyff";
