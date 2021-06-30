2095ea72c1afba8bbba19f49180ac7d44881b62e
Hasnain Raza <hasnain.mohsan@gmail.com>
2021-06-30 07:58:43 -0400

    Changed Dashboard to redirect on clientside

--- areas/account/scenes/Dashboard/index.js
+++ areas/account/scenes/Dashboard/index.js
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
+import { useRouter } from "next/router";
 
-import { selectUser } from "../../../../services/authenticatedSlice/selectors.js";
+import {
+  AuthenticationStatus,
+  selectAuthenticationStatus,
+  selectUser,
+} from "../../../../services/authenticatedSlice/selectors.js";
 
 import { Container, Row, Col } from "react-bootstrap";
 import EmailAlert from "./components/EmailAlert";
@@ -10,24 +16,45 @@ import DiscordAccountCard from "./components/DiscordAccountCard/index.js";
 import TitledPage from "../../../../components/TitledPage";
 
 export default function Dashboard() {
-  const user = useSelector(selectUser);
-  const { displayName } = user;
+  const router = useRouter();
 
-  return (
-    <TitledPage className="account-dashboard" title={`Welcome ${displayName}`}>
-      <EmailAlert />
-      <DevTrackAccountCard />
+  const authenticationStatus = useSelector(selectAuthenticationStatus);
 
-      <Container className="mt-3">
-        <Row>
-          <Col className="p-0" sm>
-            <ROBLOXAccountCard />
-          </Col>
-          <Col className="p-0" sm>
-            <DiscordAccountCard />
-          </Col>
-        </Row>
-      </Container>
-    </TitledPage>
-  );
+  useEffect(() => {
+    if (authenticationStatus === AuthenticationStatus.Unauthenticated)
+      router.push("/authentication/login");
+  }, authenticationStatus);
+
+  if (authenticationStatus === AuthenticationStatus.Authenticated) {
+    const user = useSelector(selectUser);
+    const { displayName } = user;
+
+    return (
+      <TitledPage
+        className="account-dashboard"
+        title={`Welcome ${displayName}`}
+      >
+        <EmailAlert />
+        <DevTrackAccountCard />
+
+        <Container className="mt-3">
+          <Row>
+            <Col className="p-0" sm>
+              <ROBLOXAccountCard />
+            </Col>
+            <Col className="p-0" sm>
+              <DiscordAccountCard />
+            </Col>
+          </Row>
+        </Container>
+      </TitledPage>
+    );
+  } else {
+    return (
+      <TitledPage
+        className="account-dashboard"
+        title={`Welcome ${displayName}`}
+      ></TitledPage>
+    );
+  }
 }
